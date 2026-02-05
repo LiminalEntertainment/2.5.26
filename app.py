0c004f63b4c6784ddd83032d4981e398f3f2008e
LiminalEntertainment <ve4n1x@gmail.com>
2026-02-05 10:05:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,29 @@ st.write("whats 1 + 1?")
 answer = st.text_input("enter answer: ")
 if answer == "2":
     st.write("Correct!")
+elif answer != "2":
+    st.write("Wrong")
 st.write("whats 2 * 5?")
 answer = st.text_input("enter answer: ")
 if answer == "10":
     st.write("Correct!")
+elif answer != "10":
+    st.write("Wrong")
 st.write("whats 100 / 10?")
 answer = st.text_input("enter answer: ")
 if answer == "10":
     st.write("Correct!")
+elif answer != "10":
+    st.write("Wrong")
 st.write("whats 2^5?")
 answer = st.text_input("enter answer: ")
 if answer == "32":
     st.write("Correct!")
+elif answer != "32":
+    st.write("Wrong")
 st.write("whats x equal to if: 2x + 5x = 161?")
 answer = st.text_input("enter answer: ")
 if answer == "23":
     st.write("Correct!")
+elif answer != "23":
+    st.write("Wrong")
